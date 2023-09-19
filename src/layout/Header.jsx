f166f63ba0b374bccfe47d58a57a1092bcef5c9e
VaNomad <sjur.hassel@gmail.com>
2023-09-19 23:39:37 +0100

    Builds Nav into Header component

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,32 @@
-export const Header = () => {
+import { Link, useLocation } from "react-router-dom";
+import { Nav } from "./Nav";
+import {FiHome} from "react-icons/fi"
+
+
+const Header = () => {
+  const location = useLocation();
+
+  // Conditionally render different navigation content based on the current route
+  const renderNav = () => {
+    if (location.pathname === "*") {
+      return (
+        <Link to="/" className="flex flex-col items-center">
+          <h1 className="flex items-center text-blue-800 underline underline-offset-1">
+            Go back to the <FiHome className="mx-1" /> page
+          </h1>
+        </Link>
+      );
+    } else {
+      return <Nav />;
+    }
+  };
+
   return (
-    <div>
-      <h1>HeaderComponent</h1>
-    </div>
-  )
-}
+    <header>
+      {/* Other header content */}
+      {renderNav()}
+    </header>
+  );
+};
+
+export default Header;
