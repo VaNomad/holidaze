6f775a86476d201ca085edc72518bdeacfe839ab
VaNomad <sjur.hassel@gmail.com>
2023-09-19 23:38:16 +0100

    Adds constants file for content

---
+++ src/assets/constants/constants.js
@@ -0,0 +1,96 @@
+import { holidazeLogoGreen, holidazeLogoPink, campos, kakim, loiterton, maldives1, maldives2, nickson, pixabay, roseclay, sara } from "../../assets";
+
+export const vectorList = [
+  {
+    id: 1,
+    src: holidazeLogoGreen,
+    alt: "Holidaze Logo Green",
+  },
+  {
+    id: 2,
+    src: holidazeLogoPink,
+    alt: "Holidaze Logo Pink",
+  },
+]
+export const imageList = [
+  {
+    id: 3,
+    src: campos,
+    alt: "Blue and orange sunset",
+  },
+  {
+    id: 4,
+    src: kakim,
+    alt: "Tropical Beach",
+  },
+  {
+    id: 5,
+    src: loiterton,
+    alt: "",
+  },
+  {
+    id: 6,
+    src: maldives1,
+    alt: "",
+  },
+  {
+    id: 7,
+    src: maldives2,
+    alt: "",
+  },
+  {
+    id: 8,
+    src: nickson,
+    alt: "",
+  },
+  {
+    id: 9,
+    src: pixabay,
+    alt: "",
+  },
+  {
+    id: 10,
+    src: roseclay,
+    alt: "",
+  },
+  {
+    id: 11,
+    src: sara,
+    alt: "",
+  },
+];
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "login",
+    title: "Login",
+  },
+  {
+    id: "signUp",
+    title: "Sign Up",
+  },
+  {
+    id: "profile",
+    title: "Profile",
+  },
+  {
+    id: "venueAdmin",
+    title: "Venue Admin",
+  },
+  {
+    id: "addVenue",
+    title: "Add Venue",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+  {
+    id: "logout",
+    title: "Log Out",
+  },
+]
