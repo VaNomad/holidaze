34947a214f08b76c4612a28f1735970c9bdc05b4
VaNomad <sjur.hassel@gmail.com>
2023-09-24 00:34:50 +0100

    cleaning up  venues API call

---
+++ src/api/calls/GetVenues.jsx
@@ -0,0 +1,44 @@
+import { API_BASE_URL } from "../endPoints/endpoints";
+import { useState, useEffect } from "react";
+
+export const GetVenues = () => {
+  const [venues, setVenues] = useState([]);
+  const [loading, setLoading] = useState();
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        setError(null);
+        setLoading(true);
+
+        const response = await fetch(`${API_BASE_URL}/venues?sort=created`);
+        const data = await response.json();
+        setVenues(data);
+
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        setError(true);
+      }
+    }
+
+    getData();
+  }, []);
+
+  // if (loading) {
+  //   return (
+  //     <div>Loading Venues</div>
+  //   );
+  // }
+
+  // if (error) {
+  //   return (
+  //     <div>There was an ERROR Loading Venues</div>
+  //   );
+  // }
+
+  return {venues, loading, error};
+  
+};
+
