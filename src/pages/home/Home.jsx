26587aa3a2054d35a696a79721c016f2e2903e36
VaNomad <sjur.hassel@gmail.com>
2023-09-24 00:36:26 +0100

    reimporting to Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,16 @@
 import { Helmet } from "react-helmet";
-import { Carousel } from "../../components/carousel/Carousel";
-// import { VenuesAPI } from "../../api/calls/VenuesAPI";
+import { VenuesCard } from "../../components/card/VenuesCard";
 
 export const Home = () => {
+
   return (
     <>
       <Helmet>
         <title>Holidaze | Home</title>
         <meta name="description" content="Holidaze Home" />
       </Helmet>
-      <h1 className="flex justify-center font-bold text-2xl">
-        Home
-      </h1>
-      <Carousel />
+      <h1 className="flex justify-center font-bold text-2xl">Home</h1>
+      <VenuesCard />
     </>
   );
 };
