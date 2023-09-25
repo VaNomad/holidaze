bb22a679e1fa181448db6858e7e096c237533e8e
VaNomad <sjur.hassel@gmail.com>
2023-09-25 22:03:59 +0100

    adds logs and imports carousel to Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
 import { Helmet } from "react-helmet";
 import { VenuesCard } from "../../components/card/VenuesCard";
+import { VenuesCarousel} from "../../components/carousel/venuesCarousel"
+
 
 export const Home = () => {
-
+  console.log("Venues Card", VenuesCard)
+  console.log("VenuesCarousel", VenuesCarousel)
   return (
     <>
       <Helmet>
@@ -11,6 +14,7 @@ export const Home = () => {
       </Helmet>
       <h1 className="flex justify-center font-bold text-2xl">Home</h1>
       <VenuesCard />
+      <VenuesCarousel />
     </>
   );
 };
